37eddc041ba2ba142259a54d0a774d995653362c
Brian Leung <brianleung1017@gmail.com>
2023-10-22 01:17:22 +0800

    Add PWA support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,31 @@
 import { defineConfig } from 'astro/config';
-
 import react from "@astrojs/react";
+import { VitePWA } from "vite-plugin-pwa"
+
+// Helper imports
+import { manifest } from "./utils/seoConfig"
+
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://cswbrian.github.io',
   base: '/mixologist',
-  integrations: [react()]
+  integrations: [react()],
+  vite: {
+		plugins: [
+			VitePWA({
+				registerType: "autoUpdate",
+				manifest,
+				workbox: {
+				  globDirectory: 'dist',
+				  globPatterns: [
+				    '**/*.{js,css,svg,png,jpg,jpeg,gif,webp,woff,woff2,ttf,eot,ico}',
+				  ],
+				  // Don't fallback on document based (e.g. `/some-page`) requests
+				  // This removes an errant console.log message from showing up.
+				  navigateFallback: null,
+				},
+			})
+		]
+	}
 });
