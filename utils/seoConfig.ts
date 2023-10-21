74ea8fef53f00188e2e4db23a59db83787da3393
Brian Leung <brianleung1017@gmail.com>
2023-10-22 01:52:18 +0800

    fix path

--- utils/seoConfig.ts
+++ utils/seoConfig.ts
@@ -34,17 +34,17 @@ export const manifest: Partial<ManifestOptions> = {
 	display: "minimal-ui",
 	icons: [
 		{
-			src: "/favicons/favicon-192x192.png",
+			src: "/mixologist/favicons/favicon-192x192.png",
 			sizes: "192x192",
 			type: "image/png"
 		},
 		{
-			src: "/favicons/favicon-512x512.png",
+			src: "/mixologist/favicons/favicon-512x512.png",
 			sizes: "512x512",
 			type: "image/png"
 		},
 		{
-			src: "/favicons/favicon-512x512.png",
+			src: "/mixologist/favicons/favicon-512x512.png",
 			sizes: "512x512",
 			type: "image/png",
 			purpose: "any maskable"
